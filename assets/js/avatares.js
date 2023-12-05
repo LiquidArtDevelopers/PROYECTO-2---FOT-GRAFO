f7b269b3544357a218875cf04bee26864e5f24b2
LiquidArtDevelopers <aranaz@gmail.com>
2023-12-05 21:19:53 +0100

    CLAse 10

--- assets/js/avatares.js
+++ assets/js/avatares.js
@@ -1,16 +1,18 @@
 /* recogemos en una constante (array) todos los elementos html que tenga como clase imgObjetivo. Habrá tantos como avatares */
-const objetivos = document.getElementsByClassName("imgObjetivo")
+const imgObjetivos = document.getElementsByClassName("imgObjetivo")
 
 
 /* hacemos una iteración para recorrer cada item de la constante, y escuchamos si se hace evento mouseover o mouseout sobre alguno de ellos */
-for(const objetivo of objetivos){
-    objetivo.addEventListener("mouseover", function(){
-        objetivo.style.transform="rotate(20deg)";
+for(const imgObjetivo of imgObjetivos){
+    
+    imgObjetivo.addEventListener("mouseover", function(){
+        imgObjetivo.style.transform="rotate(20deg)";
     })
     
-    objetivo.addEventListener("mouseout", function(){
-        objetivo.style.transform="rotate(0deg)";
+    imgObjetivo.addEventListener("mouseout", function(){
+        imgObjetivo.style.transform="rotate(0deg)";
     })
 }
+
 
 
